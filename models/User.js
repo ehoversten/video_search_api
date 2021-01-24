eab41657b7f9202eb2da4b08b3da617b6a11c29f
ehoversten <ehoversten@gmail.com>
2021-01-23 20:56:54 -0500

    initalized mongoDB cloud database and connection, created User model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first: {
+        type: String,
+        trim: true
+    },
+    last: {
+        type: String,
+        trim: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        required: "Username is Required"
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        match: [/.+@.\..+/, "Please Enter a valid e-mail address"],
+        required: "Email is Required",
+    },
+    password: {
+        type: String,
+        required: "Password is Required",
+        minlength: 5
+    }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const PORT = process.env.port || 3001;
 require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = express();
 
 const searchAPI = require('./utils/API');
 const axios = require("axios");
 const KEY = process.env.API_KEY;
+const db = process.env.MONGODB_URI;
 
+// -- DATABASE CONNECTION -- //
+mongoose.connect(process.env.MONGODB_URI || db || "mongodb://localhost/video_favorites", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("Mongo DB connected...");
+})
 // -- MIDDLEWARE --//
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());

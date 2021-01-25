76b9e348b1f515a2afe04a31fb2168a77471675e
ehoversten <ehoversten@gmail.com>
2021-01-24 22:13:19 -0500

    created /register route, adding User with encrypted password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 router.get('/', (req, res) => {
@@ -6,23 +9,48 @@ router.get('/', (req, res) => {
 });
 
 // -- TEST CREATE USER -- //
-router.get('/create', (req, res) => {
-    let newUser = {
-        first: 'Kire',
-        last: 'Smith',
-        username: 'bingo',
-        email: 'bingo@company.net',
-        password: 'pass1234'
-    }
+router.post('/register', async (req, res) => {
+    try {
 
-    User.create(newUser)
-        .then(user => {
-            console.log("New User Created");
-            res.json(user);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+        let { first, last, username, email, password, passwordCheck } = req.body;
+    
+        // -- VALIDATION --//
+        if(!username || !email || !password || !passwordCheck) {
+            return res.status(400).json({ msg: "Required field(s) missing" });
+        }
+        
+        if(password.length < 5) {
+            return res.status(400).json({ msg: "Password must be at least 5 characters long"});
+        }
+    
+        if(password !== passwordCheck) {
+            return res.status(400).json({ msg: "Passwords must match"});
+        }
+
+        const currentUser = await User.findOne({ email: email });
+        if(currentUser) {
+            return res.status(400).json({ msg: "A User with that email already exists"});
+        }
+
+        // Encrypt password
+        let salt = await bcrypt.genSalt(10);
+        let passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            first,
+            last,
+            username,
+            email,
+            password: passwordHash
+        });   
+
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser);
+
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    };
+
 });
 
 module.exports = router;

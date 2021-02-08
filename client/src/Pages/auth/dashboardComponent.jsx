03c2663602b969a2ee1f90408e4b7b741035f1e8
ehoversten <ehoversten@gmail.com>
2021-02-07 22:17:49 -0500

    testing react auth component

--- client/src/Pages/auth/dashboardComponent.jsx
+++ client/src/Pages/auth/dashboardComponent.jsx
@@ -1,13 +1,31 @@
-import React, { } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import UserContext from '../context/userContext';
 
+function DashboardComponent(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const { userData } = useContext(UserContext);
+    const history = useHistory();
 
-function dashboardComponent() {
-    return (
-        <>
-            <h1>Welcome Auth Dashboard</h1>
-        </>        
-    );
+    useEffect(() => {
+        console.log(userData);
+
+        if(!userData.user) {
+            history.push('/login');
+        }  else {
+            setIsLoggedIn(true);
+        }
+
+    }, []);
+
+    return isLoggedIn ? 
+        (
+            <>
+                <h1>Welcome Auth Dashboard</h1>
+                <h4>{ userData.user.username }</h4>
+            </>        
+        ) : ( <h1>Welcome Auth Dash</h1> )
 }
 
 
-export default dashboardComponent;
+export default DashboardComponent;

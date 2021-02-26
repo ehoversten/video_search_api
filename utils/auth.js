7f57cb45c6edb37aad7bc2e50d85623b1ebbb5a8
Jose <gonzalezj@protonmail.com>
2021-02-26 12:32:16 -0500

    add createToken and verifyPassword functions

--- utils/auth.js
+++ utils/auth.js
@@ -1,35 +1,59 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const isAuthorized = (req, res, next) => {
-    // -- TESTING -- //
-    console.log("Running Auth Middleware Verification ....")
-    // console.log(req.cookies);
+  // Capture Token
+  const cookie = req.cookies.token;
+  if (!cookie) {
+    return res.status(403).json({ msg: 'Token Missing, Authorization Denied' });
+  }
 
-    // Capture Token
-    const cookie = req.cookies.token;
-    if(!cookie) {
-        return res.status(403).json({ msg: "Token Missing, Authorization Denied"});
-    }
+  // const token = req.header('x-auth-token');
+  // if(!token) {
+  //     return res.status(403).json({ msg: "Token Missing, Authorization Denied"});
+  // }
 
-    // const token = req.header('x-auth-token');
-    // if(!token) {
-    //     return res.status(403).json({ msg: "Token Missing, Authorization Denied"});
-    // }
+  // --> Verify Token
+  // const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+  const verified = jwt.verify(cookie, process.env.TOKEN_SECRET);
+  if (!verified) {
+    return res.status(403).json({ msg: 'Token Failed, Authorization Denied' });
+  }
+  console.log(verified);
 
-    // --> Verify Token
-    // const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    const verified = jwt.verify(cookie, process.env.TOKEN_SECRET);
-    if(!verified) {
-        return res.status(403).json({ msg: "Token Failed, Authorization Denied"});
-    }
-    console.log(verified);
+  // Set User ID
+  req.user = verified.id;
+  console.log(req.user);
+  // Call Next middleware
+  console.log('Calling NEXT Middleware');
+  next();
+};
 
-    // Set User ID
-    req.user = verified.id;
-    console.log(req.user);
-    // Call Next middleware
-    console.log("Calling NEXT Middleware")
-    next();
-}
+const createToken = (user) => {
+  // Sign the JWT
+  //   if (!user.role) {
+  //     throw new Error('No user role specified');
+  //   }
+  return jwt.sign(
+    {
+      sub: user._id, //subject
+      user: user,
+      email: user.email,
+      role: '',
+      iss: '', //issuer
+      aud: '', //audience,
+    },
+    process.env.JWT_SECRET,
+    { algorithm: 'HS256', expiresIn: '1h' }
+  );
+};
 
-module.exports = isAuthorized;
+const verifyPassword = (passwordAttempt, hashedPassword) => {
+  return bcrypt.compare(passwordAttempt, hashedPassword);
+};
+
+module.exports = {
+  isAuthorized,
+  createToken,
+  verify,
+};

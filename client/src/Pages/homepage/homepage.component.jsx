47b33f5a3dc6cb02323b602fcbbd82d6979735c7
ehoversten <ehoversten@gmail.com>
2021-05-07 17:20:47 -0400

    fixed server error and started updating content in Homepage component

--- client/src/Pages/homepage/homepage.component.jsx
+++ client/src/Pages/homepage/homepage.component.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Row, Column, Button, Card } from 'react-bootstrap';
 
 export default function Homepage() {
   return (
-    <div>
-      <h1>Homepage!</h1>
-    </div>
+    <Container>
+      <Row className="justify-content-center">
+          <Card className="text-center m-5">
+            <Card.Header></Card.Header>
+            <Card.Body>
+              <Card.Title>Welcome to your YouTube favorite video search!</Card.Title>
+              <Card.Text>
+                Head on over to the Sign Up page and get started 
+              </Card.Text>
+              <Button variant="primary mr-3">Get Started</Button>
+              <Button variant="success">Try a Search</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">Built by @Exia and @Ehoversten</Card.Footer>
+          </Card>
+      </Row>
+    </Container>
   );
 }

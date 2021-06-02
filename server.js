da35fe8e943681f687b94581f48c41aaf0d1f86a
ehoversten <ehoversten@gmail.com>
2021-06-01 21:49:17 -0400

    removing commented out or extranous code from backend server file

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const searchAPI = require('./utils/API');
 const user_routes = require('./routes/userRoutes');
 const favorite_routes = require('./routes/favoritesRoute');
 const api_routes = require('./routes/apiRoutes');
 
 const app = express();
 app.use(cors());
-// app.use(cors({ credentials: true }));
 
 // -- DATABASE CONNECTION -- //
 mongoose.connect(
@@ -37,16 +35,12 @@ mongoose.connection.on('connected', () => {
 });
 
 // -- MIDDLEWARE --//
-app.use(express.urlencoded({ extended: false })); //Do we need this one still? We can do extended false on express.json
+app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'))
 
 // -- ROUTES -- //
-// app.get('/', (req, res) => {
-//   res.send('Hit Landing Page');
-// });
-
 app.use('/users', user_routes);
 app.use('/favorites', favorite_routes);
 app.use('/api', api_routes);
@@ -59,10 +53,6 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
 
-  // app.use(express.static(path.join(__dirname, '../build')))
-  // app.get('*', (req, res) => {
-  //     res.sendFile(path.join(__dirname, '../build'))
-  // })
 }
 
 app.listen(PORT, () => {

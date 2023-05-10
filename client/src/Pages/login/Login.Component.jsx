59d5e6042dc1805b829bab4e881a6488044a8c6d
Erik Hoversten <ehoversten@gmail.com>
2023-05-10 14:57:25 -0400

    added header option to axios calls from frontend

--- client/src/Pages/login/Login.Component.jsx
+++ client/src/Pages/login/Login.Component.jsx
@@ -66,7 +66,7 @@ function Login(props) {
 
     //-- Send to Server Route
     try {
-      let authorized = await axios.post('/users/login', { email, password });
+      let authorized = await axios.post('/users/login', { email, password }, { headers: { 'content-type': 'text/json' }});
       // -- TESTING -- //
       console.log(`Authorized:`, authorized);
 
